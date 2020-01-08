231e6f2958a0ac6852f228e823c6a529e0a2c582
iulianstave <iulian.staverescu@gmail.com>
2020-01-08 17:51:23 +0200

    Update PostList

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,19 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// use async await syntax to fetch data / posts from api
+// asynchronous action creator - takes some amount of 
+// time for it to get its data ready to go
+// - we use a middleware to make the requests
+// action must be an object or a function
+// https://github.com/reduxjs/redux-thunk
+// redux-thunk allows us to return a function as an action
+
+
+export const fetchPosts = async () => {
+  const response = await jsonPlaceholder.get('./posts');
+  
   return {
-    type: 'FETCH_POSTS'
+    type: 'FETCH_POSTS',
+    payload: response
   };
 };
