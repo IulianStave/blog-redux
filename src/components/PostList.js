11ff94290f465b8841672bca6b3e29219b794b64
iulianstave <iulian.staverescu@gmail.com>
2020-01-12 19:06:46 +0200

    Show posts through renderList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -9,11 +9,27 @@ class PostList extends React.Component {
     // call fetchPosts action creator
     this.props.fetchPosts();
   }
+
+  renderList() {
+    return this.props.posts.map( post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user"/>
+          <div className="content">
+            <div className="description">
+                <h2>{ post.title }</h2>
+                <p>{ post.body }</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props.posts);
     return (
-      <div className="ui container">
-        PostList
+      <div className="ui relaxed divided list">
+        { this.renderList() }
       </div>
     );
   }
